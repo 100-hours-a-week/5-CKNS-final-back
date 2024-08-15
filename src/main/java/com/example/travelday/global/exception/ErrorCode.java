26d0b21d3bd715d04d9df8dfd9a1c66153f5e787
euniiiii <hee9920@naver.com>
2024-08-15 21:54:56 +0900

    HOTFIX: errorcode 수정

--- src/main/java/com/example/travelday/global/exception/ErrorCode.java
+++ src/main/java/com/example/travelday/global/exception/ErrorCode.java
@@ -11,10 +11,12 @@ public enum ErrorCode {
     // 호텔 관련 오류
     FAIL_TO_GET_HOTEL_INFO(HttpStatus.INTERNAL_SERVER_ERROR, "HT001", "호텔 정보를 가져오는데 실패하였습니다."),
 
+    // 항공권 관련 오류
+    FAIL_TO_GET_FLIGHT_INFO(HttpStatus.INTERNAL_SERVER_ERROR,"FL001", "항공권 오류"),
+
     // 서버 오류
-    SEVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SV001", "서버 오류"),
-    FAIL_TO_GET_HOTEL_INFO(HttpStatus.INTERNAL_SERVER_ERROR,"SV002", "호텔 오류"),
-    FAIL_TO_GET_FLIGHT_INFO(HttpStatus.INTERNAL_SERVER_ERROR,"SV003", "항공권 오류");
+    SEVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SV001", "서버 오류");
+
 
     private final HttpStatus httpStatus;
 
