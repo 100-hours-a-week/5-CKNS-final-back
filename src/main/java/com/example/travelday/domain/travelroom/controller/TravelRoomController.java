6474aab9d68d5811cfddadedb3fa2231eec23e83
sean <jh9826@jbnu.ac.kr>
2024-08-21 16:34:46 +0900

    feat: 여행방 생성, 삭제, 목록 조회, 단일 조회 구현

--- src/main/java/com/example/travelday/domain/travelroom/controller/TravelRoomController.java
+++ src/main/java/com/example/travelday/domain/travelroom/controller/TravelRoomController.java
@@ -1,49 +1,58 @@
 package com.example.travelday.domain.travelroom.controller;
 
-import com.example.travelday.domain.travelroom.dto.TravelRoomDto;
-import com.example.travelday.domain.travelroom.repository.TravelRoomRepository;
+import com.example.travelday.domain.travelroom.dto.request.TravelRoomCreateReqDto;
+import com.example.travelday.domain.travelroom.dto.response.TravelRoomResDto;
+import com.example.travelday.domain.travelroom.service.TravelRoomService;
+import com.example.travelday.global.common.ApiResponseEntity;
+import com.example.travelday.global.common.ResponseText;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/rooms")
 @RequiredArgsConstructor
 public class TravelRoomController {
 
-    private final TravelRoomRepository travelRoomRepository;
+    private final TravelRoomService travelRoomService;
 
     /**
-     * 여행 방 목록 조회
+     * 여행방 목록 조회
      */
-    @GetMapping()
-    public List<TravelRoomDto> room() {
-        return travelRoomRepository.findAllRoom();
+    @GetMapping
+    public ResponseEntity<ApiResponseEntity<List<TravelRoomResDto>>> getAllTravelRoom() {
+        List<TravelRoomResDto> travelRooms = travelRoomService.getAllTravelRoom();
+        return ResponseEntity.ok(ApiResponseEntity.of(travelRooms));
+    }
+
+    /**1
+     * 여행방 단일 조회
+     */
+    @GetMapping("/{travelRoomId}")
+    public ResponseEntity<ApiResponseEntity<TravelRoomResDto>> getTravelRoom(@PathVariable Long travelRoomId) {
+        TravelRoomResDto travelRoom = travelRoomService.getTravelRoomById(travelRoomId);
+        return ResponseEntity.ok(ApiResponseEntity.of(travelRoom));
     }
 
     /**
-    * 여행 방 단일 조회
+     * 여행방 생성
      */
-    @GetMapping("/{travel_room_id}")
-    public TravelRoomDto roomInfo(@PathVariable String travel_room_id) {
-        return travelRoomRepository.findRoomById(travel_room_id);
+    @PostMapping
+    public ResponseEntity<ApiResponseEntity<String>> createTravelRoom(@RequestBody TravelRoomCreateReqDto requestDto) {
+        travelRoomService.createTravelRoom(requestDto);
+        return ResponseEntity.ok(ApiResponseEntity.of(ResponseText.SUCCESS_CREATE_TRAVELROOM));
     }
 
     /**
-     * 여행 방 생성
+     * 여행방 삭제
      */
-    @PostMapping()
-    public TravelRoomDto createRoom(@RequestParam String name) {
-        return travelRoomRepository.createTravelRoom(name);
+    @DeleteMapping("/{travelRoomId}")
+    public ResponseEntity<ApiResponseEntity<Void>> deleteTravelRoom(@PathVariable Long travelRoomId) {
+        travelRoomService.deleteTravelRoom(travelRoomId);
+        return ResponseEntity.ok(ApiResponseEntity.of(null));
     }
-
-    /**
-     * 여행 방 삭제
-     */
-    @DeleteMapping("/{travel_room_id}")
-    public void deleteRoom(@PathVariable String travel_room_id) {
-        travelRoomRepository.deleteRoom(travel_room_id);
-    }
-
 }
