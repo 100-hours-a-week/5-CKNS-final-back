35e5d711ab83fe9b2b40947d8e36672ba8140dcb
sean <jh9826@jbnu.ac.kr>
2024-08-21 16:34:24 +0900

    feat: ResponseText create TravelRoom 추가

--- src/main/java/com/example/travelday/global/common/ResponseText.java
+++ src/main/java/com/example/travelday/global/common/ResponseText.java
@@ -7,4 +7,6 @@ import lombok.NoArgsConstructor;
 public class ResponseText {
 
     public static final String OK = "OK";
+
+    public static final String SUCCESS_CREATE_TRAVELROOM = "여행방 생성 성공";
 }
