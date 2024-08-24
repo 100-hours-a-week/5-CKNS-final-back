12328fe39af91710494cb18209d7136763e8460b
sean <jh9826@jbnu.ac.kr>
2024-08-24 21:40:49 +0900

    feat: raw_flight_data mysql 에 삽입 로직 구현

--- src/main/java/com/example/travelday/domain/supersale/service/FlightService.java
+++ src/main/java/com/example/travelday/domain/supersale/service/FlightService.java
@@ -3,6 +3,8 @@ package com.example.travelday.domain.supersale.service;
 import com.amadeus.exceptions.ResponseException;
 import com.amadeus.resources.FlightOfferSearch;
 import com.example.travelday.domain.supersale.dto.response.FlightResDto;
+import com.example.travelday.domain.supersale.entity.FlightOffer;
+import com.example.travelday.domain.supersale.repository.FlightOfferRepository;
 import com.example.travelday.domain.supersale.utils.AmadeusConnect;
 import com.example.travelday.global.exception.CustomException;
 import com.example.travelday.global.exception.ErrorCode;
@@ -28,6 +30,7 @@ public class FlightService {
     private final AmadeusConnect amadeusConnect;
     private final RedisTemplate<String, Object> redisTemplate;
     private final Gson gson = new Gson();
+    private final FlightOfferRepository flightOfferRepository;
 
     @Value("${spring.data.redis.timeout}")
     private long redisTTL;
@@ -47,8 +50,20 @@ public class FlightService {
                 return cachedData;
             }
 
+            FlightOfferSearch[] flightOffersJson = amadeusConnect.flights(origin, destination, departDate, adults, returnDate);
+
+            log.info("11111111111FlightService.getFlightOffers: {}", flightOffersJson);
+
+            // flightOffersJson을 JSON 문자열로 변환
+            String flightOffersJsonString = gson.toJson(flightOffersJson);
+
+            // flightOffersJsonString을 MySQL에 저장
+            FlightOffer flightOfferEntity = new FlightOffer(flightOffersJsonString);
+            flightOfferRepository.save(flightOfferEntity);
+
+
             // Redis 데이터가 없거나 문제가 있을 경우 Amadeus API 호출
-            List<FlightOfferSearch> flightOffers = List.of(amadeusConnect.flights(origin, destination, departDate, adults, returnDate));
+            List<FlightOfferSearch> flightOffers = List.of(flightOffersJson);
             List<FlightResDto> flightResDtos = new ArrayList<>();
 
             for (FlightOfferSearch flight : flightOffers) {
