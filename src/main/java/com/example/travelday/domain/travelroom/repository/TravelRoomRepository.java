2a199c67c87047c316f700699bdb55b43eb99dd9
sean <jh9826@jbnu.ac.kr>
2024-08-25 14:41:45 +0900

    feat: AuthenticationPrincipal 추가 로 TravelRoomRepository 수정

--- src/main/java/com/example/travelday/domain/travelroom/repository/TravelRoomRepository.java
+++ src/main/java/com/example/travelday/domain/travelroom/repository/TravelRoomRepository.java
@@ -1,7 +1,17 @@
 package com.example.travelday.domain.travelroom.repository;
 
+import com.example.travelday.domain.auth.entity.Member;
 import com.example.travelday.domain.travelroom.entity.TravelRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TravelRoomRepository extends JpaRepository<TravelRoom, Long> {
+
+    // 특정 사용자와 연관된 모든 TravelRoom 조회
+    List<TravelRoom> findAllByMember(Member member);
+
+    // 특정 사용자와 연관된 특정 TravelRoom 조회
+    Optional<TravelRoom> findByIdAndMember(Long id, Member member);
 }
