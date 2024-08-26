e21208beb31d06c045de653df40644eddb398ca0
sean <jh9826@jbnu.ac.kr>
2024-08-26 11:09:00 +0900

    hotfix: amadeus 데이터 삽입 요청 수정

--- src/main/java/com/example/travelday/domain/supersale/scheduler/MultiDestinationFlightScheduler.java
+++ src/main/java/com/example/travelday/domain/supersale/scheduler/MultiDestinationFlightScheduler.java
@@ -19,13 +19,13 @@ public class MultiDestinationFlightScheduler {
 
     private final FlightService flightService;
 
-    @Scheduled(cron = "0 0 1 * * ?")
+    //    @Scheduled(cron = "0/20 * * * * ?")
+    @Scheduled(cron = "0/20 * * * * ?")
     public void fetchAndStoreFlightOffers() {
 
         log.info("===== fetchAndStoreFlightOffers for Multiple Destinations =====");
 
-        String originICN = "ICN";
-        String destination = "ICN";
+        String icn= "ICN";
         String adults = "1";
 
         // 대상 목적지 목록
@@ -36,18 +36,19 @@ public class MultiDestinationFlightScheduler {
         };
 
         // 현재 날짜 기준 하루 뒤 편도
-        LocalDate departureDate = LocalDate.now().plusDays(2);
+        LocalDate departureDate = LocalDate.now().plusDays(1);
         String departDate = departureDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
 
         // 각 목적지에 대해 검색 및 저장
         // ICN에서 출발하는 각 공항으로 검색 및 저장
         for (String airport : airportList) {
             try {
-                flightService.getFlightOffers(originICN, airport, departDate, adults);
-                flightService.getFlightOffers(airport, originICN, departDate, adults);
-                log.info("Successfully fetched and stored flight data for destination from ICN to: {}", destination);
+                flightService.getFlightOffers(icn, airport, departDate, adults);
+                log.info("Successfully fetched and stored flight data for destination from ICN to: {}", airport);
+                flightService.getFlightOffers(airport, icn, departDate, adults);
+                log.info("Successfully fetched and stored flight data for destination to ICN to: {}", airport);
             } catch (ResponseException e) {
-                log.error("Failed to fetch flight data from ICN to destination: {} - {}", destination, e.getMessage());
+                log.error("Failed to fetch flight data from ICN to destination: {} - {}", airport, e.getMessage());
                 throw new CustomException(ErrorCode.FAIL_TO_GET_FLIGHT_INFO);
             }
         }
