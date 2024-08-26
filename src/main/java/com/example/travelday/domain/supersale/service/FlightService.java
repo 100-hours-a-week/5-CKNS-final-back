1a748600a100b9aecab9a6bb10bf973cb4ed3f14
euniiiii <hee9920@naver.com>
2024-08-26 14:29:06 +0900

    HOTFIX: 데이터 오류 수정

--- src/main/java/com/example/travelday/domain/supersale/service/FlightService.java
+++ src/main/java/com/example/travelday/domain/supersale/service/FlightService.java
@@ -42,7 +42,6 @@ public class FlightService {
             String redisKey = "flightOffer:" + origin + ":"+ destination + ":" + departDate;
 
             ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-            String cachedDataJson = (String) valueOperations.get(redisKey);
 
             FlightOfferSearch[] flightOffersJson = amadeusConnect.flights(origin, destination, departDate, adults);
 
