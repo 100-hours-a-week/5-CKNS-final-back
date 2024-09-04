b698463941c09f26dcc2961eb1e4f869cd9163e7
sean <jh9826@jbnu.ac.kr>
2024-09-04 14:42:25 +0900

    refactor: 불필요한 파일 삭제

--- src/main/java/com/example/travelday/global/config/EnvConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.travelday.global.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-//@PropertySource("file:${user.dir}/.env")
-public class EnvConfig {
-
-}
--- src/main/java/com/example/travelday/global/config/SentryConfig.java
+++
@@ -1,9 +0,0 @@
-//package com.example.travelday.global.config.security;
-//
-//import io.sentry.spring.jakarta.EnableSentry;
-//import org.springframework.context.annotation.Configuration;
-//
-//@EnableSentry(dsn = "https://03037d8f733436db9cc28b8e01b367f3@o4507814026543104.ingest.us.sentry.io/4507825093607424")
-//@Configuration
-//public class SentryConfig {
-//}
