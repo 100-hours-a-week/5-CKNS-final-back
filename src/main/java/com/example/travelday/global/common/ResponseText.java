51a228c97293d8c512e3d4de2bf2b21786fd592b
sean <jh9826@jbnu.ac.kr>
2024-09-04 16:30:17 +0900

    refactor: 여행방 삭제 텍스트 추가 및 여행일정과 여행방 정렬

--- src/main/java/com/example/travelday/global/common/ResponseText.java
+++ src/main/java/com/example/travelday/global/common/ResponseText.java
@@ -18,7 +18,7 @@ public class ResponseText {
     // travelroom
     public static final String SUCCESS_CREATE_TRAVELROOM = "여행방 생성 성공";
     public static final String SUCCESS_UPDATE_TRAVELROOM = "여행방 수정 성공";
-    public static final String SUCCESS_DELETE_TRAVELPLAN = "여행일정 삭제 성공";
+    public static final String SUCCESS_DELETE_TRAVELROOM = "여행 일정 목록 삭제 성공";
 
     // member
     public static final String SUCCESS_LOGOUT = "로그아웃 성공";
@@ -28,4 +28,5 @@ public class ResponseText {
     public static final String SUCCESS_CREATE_TRAVELPLAN = "여행 일정 생성 성공";
     public static final String SUCCESS_UPDATE_TRAVELPLAN = "여행 일정 갱신 성공";
     public static final String SUCCESS_CREATE_TRAVELPLANLIST = "여행 일정 목록 생성 성공";
+    public static final String SUCCESS_DELETE_TRAVELPLAN = "여행일정 삭제 성공";
 }
