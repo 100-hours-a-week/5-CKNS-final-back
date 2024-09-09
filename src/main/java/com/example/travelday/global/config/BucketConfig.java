af3f2170746c2986ce114e93aacbb023be128842
sean <jh9826@jbnu.ac.kr>
2024-09-09 22:00:36 +0900

    chore: REFILL TOKEN 수 변경

--- src/main/java/com/example/travelday/global/config/BucketConfig.java
+++ src/main/java/com/example/travelday/global/config/BucketConfig.java
@@ -3,6 +3,7 @@ package com.example.travelday.global.config;
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Refill;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,12 +12,15 @@ import java.time.Duration;
 @Configuration
 public class BucketConfig {
 
+    @Value("${bucket.max-tokens}")
+    private long MAX_BUCKET_TOKENS;
+
     @Bean
     public Bucket bucket() {
 
-        final Refill refill = Refill.intervally(40, Duration.ofDays(1));
+        final Refill refill = Refill.intervally(MAX_BUCKET_TOKENS, Duration.ofDays(1));
 
-        final Bandwidth limit = Bandwidth.classic(40, refill);
+        final Bandwidth limit = Bandwidth.classic(MAX_BUCKET_TOKENS, refill);
 
         return Bucket.builder()
                     .addLimit(limit)
