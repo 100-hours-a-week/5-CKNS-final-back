6947061a32d78316e1dcf15539a0e2a256caa56c
sean <jh9826@jbnu.ac.kr>
2024-09-11 14:52:11 +0900

    chore: 컨플릭트 해결

--- src/main/java/com/example/travelday/TravelDayApplication.java
+++ src/main/java/com/example/travelday/TravelDayApplication.java
@@ -4,7 +4,6 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.time.LocalDateTime;
 import java.util.TimeZone;
 
