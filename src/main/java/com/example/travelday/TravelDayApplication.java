1a3eda9d8dd80f07f7a8f4b9f7ec219e44cbb829
sean <jh9826@jbnu.ac.kr>
2024-09-11 15:02:39 +0900

    chore: application 컨플릭트 해결

--- src/main/java/com/example/travelday/TravelDayApplication.java
+++ src/main/java/com/example/travelday/TravelDayApplication.java
@@ -4,10 +4,13 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 import java.time.LocalDateTime;
 import java.util.TimeZone;
 
 @Slf4j
+@EnableScheduling
 @SpringBootApplication
 public class TravelDayApplication {
 
