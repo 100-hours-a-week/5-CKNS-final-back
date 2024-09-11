6b74bfcc19064500b7938cb378fc286fcdcb1523
sean <jh9826@jbnu.ac.kr>
2024-09-11 13:43:19 +0900

    chore: MongoConfig 추가

---
+++ src/main/java/com/example/travelday/global/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.example.travelday.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
