8c55a04967914ad551899d8b8883adc0a036f43a
sean <jh9826@jbnu.ac.kr>
2024-09-11 15:21:27 +0900

    chore: MongoConfig 구성

--- src/main/java/com/example/travelday/domain/chat/repository/ChatRepository.java
+++ src/main/java/com/example/travelday/domain/chat/repository/ChatRepository.java
@@ -2,9 +2,11 @@ package com.example.travelday.domain.chat.repository;
 
 import com.example.travelday.domain.chat.entity.Chat;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ChatRepository extends MongoRepository<Chat, String> {
     List<Chat> findAllByTravelRoomId(Long travelRoomId);
 }
