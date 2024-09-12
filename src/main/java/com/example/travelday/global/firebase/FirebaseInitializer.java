a05ad7e17e3dbfa57ebefa28f7f7348a278eb70d
ss0ming <hee9920@naver.com>
2024-09-12 21:29:45 +0900

    refactor: 로그 메세지 출력 수정

--- src/main/java/com/example/travelday/global/firebase/FirebaseInitializer.java
+++ src/main/java/com/example/travelday/global/firebase/FirebaseInitializer.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -11,6 +12,7 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.IOException;
 import java.io.InputStream;
 
+@Slf4j
 @Configuration
 public class FirebaseInitializer {
 
@@ -35,7 +37,7 @@ public class FirebaseInitializer {
                 FirebaseApp.initializeApp(options);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
     }
 }
