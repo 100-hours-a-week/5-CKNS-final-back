65dfab2f1b54e666b727770d15279f8289e9349b
sean <jh9826@jbnu.ac.kr>
2024-09-13 02:40:24 +0900

    TEST: TEST2 timezone 2 46

--- src/main/java/com/example/travelday/TravelDayApplication.java
+++ src/main/java/com/example/travelday/TravelDayApplication.java
@@ -3,10 +3,12 @@ package com.example.travelday;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.LocalDateTime;
 
 @Slf4j
+@EnableScheduling
 @SpringBootApplication
 public class TravelDayApplication {
 
