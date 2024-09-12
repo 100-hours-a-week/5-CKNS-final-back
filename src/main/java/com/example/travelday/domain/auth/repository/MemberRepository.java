6ad0da4dc4da40097eba72c76560c13854d49971
ss0ming <hee9920@naver.com>
2024-09-12 21:30:27 +0900

    feat: 회원 검색 기능

--- src/main/java/com/example/travelday/domain/auth/repository/MemberRepository.java
+++ src/main/java/com/example/travelday/domain/auth/repository/MemberRepository.java
@@ -1,7 +1,10 @@
 package com.example.travelday.domain.auth.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.travelday.domain.auth.entity.Member;
@@ -14,4 +17,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUserIdAndSocialType(String userId, SocialType socialType);
 
     boolean existsByNickname(String nickname);
+
+    Page<Member> findByNicknameContaining(String keyword, Pageable pageable);
 }
