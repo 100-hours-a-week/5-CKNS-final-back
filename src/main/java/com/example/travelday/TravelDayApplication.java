027b75d714f1ed8bb33c6a9e3f03739a82ecbf28
sean <jh9826@jbnu.ac.kr>
2024-09-13 02:11:21 +0900

    TEST: TEST2 timezone

--- src/main/java/com/example/travelday/TravelDayApplication.java
+++ src/main/java/com/example/travelday/TravelDayApplication.java
@@ -1,12 +1,8 @@
 package com.example.travelday;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDateTime;
-import java.util.TimeZone;
 
 @Slf4j
 @SpringBootApplication
@@ -14,12 +10,6 @@ public class TravelDayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelDayApplication.class, args);
-    }
-
-    @PostConstruct
-    public void init() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-        log.info("Spring boot application running in UTC timezone : {}", LocalDateTime.now());
     }
 }
 
