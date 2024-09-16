4b6a39c2291c37672225fe4b7763d19f0a121e9c
sean <jh9826@jbnu.ac.kr>
2024-09-17 01:07:15 +0900

    chore: cors 설정 및 암호화

--- src/main/java/com/example/travelday/global/config/WebSocketConfig.java
+++ src/main/java/com/example/travelday/global/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.example.travelday.global.config;
 
 import com.example.travelday.domain.chat.utils.StompHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -16,16 +17,28 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final StompHandler stompHandler;
 
+    @Value("${websocket.allowed-origins}")
+    private String allowedOrigins;
+
+    @Value("${websocket.destination-prefix}")
+    private String destinationPrefixes;
+
+    @Value("${websocket.simple-broker}")
+    private String simpleBroker;
+
+    @Value("${websocket.endpoint}")
+    private String endpoint;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker(simpleBroker);
+        registry.setApplicationDestinationPrefixes(destinationPrefixes);
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-            .setAllowedOrigins("*") // TODO: CORS 설정
+        registry.addEndpoint(endpoint)
+            .setAllowedOrigins(allowedOrigins.split(","))
             .withSockJS();
     }
 
