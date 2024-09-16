d7615a5488de9fb3617be2e13b1403fec99c247a
sean <jh9826@jbnu.ac.kr>
2024-09-16 19:58:54 +0900

    chore: 정산 에러코드 추가

--- src/main/java/com/example/travelday/global/exception/ErrorCode.java
+++ src/main/java/com/example/travelday/global/exception/ErrorCode.java
@@ -31,6 +31,9 @@ public enum ErrorCode {
     // travel plan error
     TRAVEL_PLAN_NOT_FOUND(HttpStatus.BAD_REQUEST, "TP001", "존재하지 않는 여행 일정입니다."),
 
+    // settlement error
+    SETTLEMENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "ST001", "존재하지 않는 정산입니다."),
+
     // user error
     NICKNAME_ALREADY_TAKEN(HttpStatus.CONFLICT, "MB001", "이미 사용 중인 닉네임입니다."),
     NICKNAME_CREATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "MB002", "닉네임 생성에 실패했습니다."),
