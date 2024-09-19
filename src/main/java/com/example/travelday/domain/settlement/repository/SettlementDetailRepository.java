0ab4f1b4a004142dddcf1e8e4f56cb8bae9faf53
sean <jh9826@jbnu.ac.kr>
2024-09-19 14:27:56 +0900

    Refactor: @Repository 제거

--- src/main/java/com/example/travelday/domain/settlement/repository/SettlementDetailRepository.java
+++ src/main/java/com/example/travelday/domain/settlement/repository/SettlementDetailRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface SettlementDetailRepository extends JpaRepository<SettlementDetail, Long> {
     List<SettlementDetail> findBySettlementId(Long settlementId);
 }
