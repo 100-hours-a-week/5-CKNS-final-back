92f04fc83bfad3b6603defeb97dbbd844e94da86
HidenLee <hyundo1995@gmail.com>
2024-09-24 15:15:25 +0900

    HOTFIX: git merge conflict 해결

--- src/main/java/com/example/travelday/domain/auth/repository/MemberRepository.java
+++ src/main/java/com/example/travelday/domain/auth/repository/MemberRepository.java
@@ -17,4 +17,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByNickname(String nickname);
 
     List<Member> findByNicknameContaining(String keyword);
+
+    Optional<Member> findByNickname(String nickname);
 }
